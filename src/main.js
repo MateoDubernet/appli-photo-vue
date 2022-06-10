c09edecb8bbffe81c1436bf28c771ffa4d7b9775
Mateo <mateo.dubernet@cogis.com>
2022-06-10 19:38:35 +0200

    restore all files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+/* 
+    [IMPORTS] Vue.js
+    Import main modules to define App
+*/
+    // Vue imports
+    import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+    // App imports
+    import App from './App.vue';
+    import Appstore from './store/index'
+    import Approuter from './router';
+//
+
+/* 
+    [APP] Launch
+    Init new Vue.js App
+*/
+    createApp(App)
+    .use( Appstore ) //=> Inject Appstore has a middleware
+    .use( Approuter ) //=> Inject AppRouter has a middleware
+    .mount('#appli-photo-vue');
+//
